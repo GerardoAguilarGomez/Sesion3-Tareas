c5f785aa6b0158402dd098d4acda052ba42c81d4
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-23 12:57:38 +0100

    subir

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 4;
+
+            Console.WriteLine("La suma es {0}", a + b);
+            Console.WriteLine("La resta es {0}", a - b);
+            Console.WriteLine("La multi es {0}", a * b);
+            Console.WriteLine("La división es {0}", a / b);
+            Console.WriteLine("El resto es {0}", a % b);
+
+        }
+    }
+}
