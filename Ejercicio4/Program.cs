95ecf0978d43e1224b600ce687c0fc7401c58a3b
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-23 13:25:24 +0100

    ejer 4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+
+            Console.WriteLine("Valor inicial variable n: {0}", n);
+            n = n + 77;
+            Console.WriteLine("N+77={0}",n);
+            n = n - 3;
+            Console.WriteLine("N-3={0}", n);
+            n = n * 2;
+            Console.WriteLine("Nx2={0}", n);
+        }
+    }
+}
