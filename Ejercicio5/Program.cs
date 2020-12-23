2663381af9fcfaf6258f2c0197a736e2866dc293
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-23 13:31:07 +0100

    ejer 5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = 7;
+            int d = 9;
+
+            Console.WriteLine("La variable a es igual a {0}", a);
+            Console.WriteLine("La variable b es igual a {0}", b);
+            Console.WriteLine("La variable c es igual a {0}", c);
+            Console.WriteLine("La variable d es igual a {0}", d);
+
+            b = c;
+            c = a;
+            a = d;
+            d = b;
+
+            Console.WriteLine("La variable a es igual a {0}", a);
+            Console.WriteLine("La variable b es igual a {0}", b);
+            Console.WriteLine("La variable c es igual a {0}", c);
+            Console.WriteLine("La variable d es igual a {0}", d);
+        }
+    }
+}
